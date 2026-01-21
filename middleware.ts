33c05dd145ddfa1dbabb740b38a94b721b009844
Tom Apperley <tomapperley8-del@users.noreply.github.com>
2026-01-21 16:20:14 +0000

    fix: allow public access to .html files in middleware

--- middleware.ts
+++ middleware.ts
@@ -12,8 +12,8 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * - public folder
+     * - public folder (images and html files)
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|html)$).*)',
   ],
 }
